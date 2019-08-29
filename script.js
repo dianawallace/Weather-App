aff2df31353a50b54f903bbb497b82363838ac73
Diana Wallace <dianawallacedeveloper@gmail.com>
2019-08-29 17:57:05 -0600

    created searchWeather function

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 
 let units = 'imperial';
 let searchMethod;
+
+function searchWeather(searchTerm) {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`).then(result => {
+        return result.json();
+    }).then(result => {
+        Init(result);
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
