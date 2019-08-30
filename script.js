a72e8849e13d320cb672b6dd37869eeb5145f96a
Diana Wallace <dianawallacedeveloper@gmail.com>
2019-08-30 17:08:45 -0600

    created init function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 let units = 'imperial';
-let searchMethod;
+let searchMethod = 'zip';
 
 function searchWeather(searchTerm) {
     fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`).then(result => {
@@ -10,7 +10,9 @@ function searchWeather(searchTerm) {
     })
 }
 
-
+function init(resultFromServer) {
+    console.log(resultFromServer);
+}
 
 
 
