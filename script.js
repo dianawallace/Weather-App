7867283d05d4e4979909c7f97d2e732701e73763
Diana Wallace <dianawallacedeveloper@gmail.com>
2019-09-03 17:56:25 -0600

    created switch statement within init function

--- script.js
+++ script.js
@@ -19,7 +19,32 @@ function searchWeather(searchTerm) {
 }
 
 function init(resultFromServer) {
-   console.log(resultFromServer); 
+   switch (resultFromServer.weather[0].main) {
+       case 'Clear':
+            document.body.style.backgroundImage = 'url("clear.jpg")';
+            break;
+
+        case 'Clouds':
+            document.body.style.backgroundImage = 'url("cloudy.jpg")';
+            break;
+        
+        case 'Rain':
+        case 'Drizzle':
+        case 'Mist':
+                document.body.style.backgroundImage = 'url("rain.jpg")';
+            break;
+
+        case 'Thunderstorm':
+                document.body.style.backgroundImage = 'url("storm.jpg")';
+            break;
+
+        case 'Snow':
+                document.body.style.backgroundImage = 'url("snow.jpg")';
+            break;
+
+        default:
+            break;
+   }
 }
 
 document.getElementById('searchBtn').addEventListener('click', () => {
